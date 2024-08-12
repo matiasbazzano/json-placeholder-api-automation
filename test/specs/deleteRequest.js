132685661ef7f03f0baaab1bd343a13d63b45854
Matias Bazzano <matiasbazzano19@gmail.com>
2024-08-12 18:01:31 -0300

    Create deleteRequest.js

---
+++ test/specs/deleteRequest.js
@@ -0,0 +1,18 @@
+import { apiHelper } from '../utils/apiHelper.js';
+import functions from '../utils/functions.js';
+import { API_ENDPOINTS } from '../config/constants.js';
+
+describe('API DELETE Requests', () => {
+
+    it('DELETE with valid ID', async () => {
+        const response = await apiHelper.delete(`${API_ENDPOINTS.COMMENTS}/05`);
+        functions.expectSuccessResponseStatusCode(response.status);
+        functions.expectEmptyResponseBody(response.body);
+    });
+
+    it('DELETE with invalid ID', async () => {
+        const response = await apiHelper.get(API_ENDPOINTS.COMMENTS + '/test');
+        functions.expectSuccessResponseStatusCode(response.status);
+        functions.expectEmptyResponseBody(response.body);
+    });
+});
